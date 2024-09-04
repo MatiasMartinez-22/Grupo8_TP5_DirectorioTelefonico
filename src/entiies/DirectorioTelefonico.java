7722e6c2d598ca2547e000cfd36a070adca9d5af
MatiasMartinez-22 <matias.e.martinez1993@gmail.com>
2024-09-04 20:21:16 -0300

    Hi

---
+++ src/entiies/DirectorioTelefonico.java
@@ -0,0 +1,24 @@
+/*
+Un directorio telefónico posee una lista de Contactos de los que interesa conocer su DNI, nombre, apellido, ciudad 
+y dirección. Un contacto se empareja con un teléfono tipo Long.
+El directorio telefónico posee además las siguientes funcionalidades:
+A. agregarContacto() que permite registrar un nuevo contacto con su respectivo nro. de teléfono. Siendo el nro.
+del teléfono la clave del mismo.
+B. buscarContacto() que en base al nro. de teléfono retorna el Contacto asociado al mismo.
+C. buscarTeléfono() que en base a un apellido nos devuelve un Set<Long> con los números de teléfono asociados
+a dicho apellido.
+D. buscarContactos() que en base a una ciudad nos devuelve un ArrayList con los Contactos asociados a dicha ciudad.
+E. borrarContacto() que en base al número de teléfono elimina el contacto del directorio.
+Importante: Armar el modelo UML representado las clases necesarias. Implementar en java con TreeMap<Long, 
+Cliente> y TreeSet. Recuerde el uso de: Iterator y keySet()
+ */
+package entiies;
+
+/**
+ *
+ * @author matia
+ */
+public class DirectorioTelefonico {
+    
+    
+}
