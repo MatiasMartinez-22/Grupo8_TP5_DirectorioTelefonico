df6eb5c080da04556714c20a65f4ec385039fa9b
MatiasMartinez-22 <matias.e.martinez1993@gmail.com>
2024-09-05 18:49:18 -0300

    mmm

--- src/entiies/Contacto.java
+++ src/entiies/Contacto.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Un directorio telefónico posee una lista de Contactos de los que interesa conocer su DNI, nombre, apellido, ciudad 
+y dirección. Un contacto se empareja con un teléfono tipo Long.
  */
 package entiies;
 
@@ -9,5 +9,66 @@ package entiies;
  * @author matia
  */
 public class Contacto {
+    
+    private Integer dni;
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String direccion;
+
+    public Contacto(Integer dni, String nombre, String apellido, String ciudad, String direccion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" + "dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", ciudad=" + ciudad + ", direccion=" + direccion + '}';
+    }
+    
+    
     
 }
--- src/entiies/DirectorioTelefonico.java
+++ src/entiies/DirectorioTelefonico.java
@@ -2,9 +2,8 @@
 Un directorio telefónico posee una lista de Contactos de los que interesa conocer su DNI, nombre, apellido, ciudad 
 y dirección. Un contacto se empareja con un teléfono tipo Long.
 El directorio telefónico posee además las siguientes funcionalidades:
-A. agregarContacto() que permite registrar un nuevo contacto con su respectivo nro. de teléfono. Siendo el nro.
-del teléfono la clave del mismo.
-B. buscarContacto() que en base al nro. de teléfono retorna el Contacto asociado al mismo.
+
+
 C. buscarTeléfono() que en base a un apellido nos devuelve un Set<Long> con los números de teléfono asociados
 a dicho apellido.
 D. buscarContactos() que en base a una ciudad nos devuelve un ArrayList con los Contactos asociados a dicha ciudad.
@@ -14,11 +13,58 @@ Cliente> y TreeSet. Recuerde el uso de: Iterator y keySet()
  */
 package entiies;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *
  * @author matia
  */
-public class DirectorioTelefonico {
+public class DirectorioTelefonico  {
     
+    private TreeMap <  Long , Contacto > contactoTreeMap ;
+    private Long numeroTel;
+
+    public DirectorioTelefonico() {
+      
+        this.contactoTreeMap=  new TreeMap ();
+        
+    }
+
+    public TreeMap<Long, Contacto> getContactoTreeMap() {
+        return contactoTreeMap;
+    }
+
+    public Long getNumeroTel() {
+        return numeroTel;
+    }
+
+   
+    /* A. agregarContacto() que permite registrar un nuevo contacto con su respectivo nro. de teléfono. Siendo el nro.
+del teléfono la clave del mismo. */
+    
+    public void agregarContacto(Contacto contact , Long tel) {
+        
+        contactoTreeMap.put(tel, contact);
+        
+    }
+    
+    
+    /*B. buscarContacto() que en base al nro. de teléfono retorna el Contacto asociado al mismo.*/
+    
+    public Contacto buscarContacto( Long tel) {
+  
+        for (Map.Entry<Long, Contacto> entry : contactoTreeMap.entrySet()) {
+            Object key = entry.getKey();
+            Object val = entry.getValue();
+            
+            if(key.equals(val)){
+                return entry.getValue();
+            }
+            
+            
+        }
+    return null;
+    }
     
 }
